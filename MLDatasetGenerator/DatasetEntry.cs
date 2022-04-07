311e6fcde1a5c8003b0f5ca5d090a641fbdaead3
TeamDoodz <unconfigured@null.spigotmc.org>
2022-04-07 15:41:11 -0500

    dataset generator init

---
+++ MLDatasetGenerator/DatasetEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLDatasetGenerator {
+	public struct DatasetEntry {
+		public int Health;
+		public int Attack;
+		public float PercentGimmick;
+		public float PercentOffensive;
+		public float PercentDefensive;
+		public float PercentUtility;
+		public int PowerLevel;
+		public int CostTier;
+		public bool IsPart3Random;
+
+		public override string ToString() {
+			return $"{Health}	{Attack}	{PercentGimmick}	{PercentDefensive}	{PercentOffensive}	{PercentUtility}	{PowerLevel}	{CostTier}	{(IsPart3Random? "1" : "0")}";
+		}
+	}
+}
---
+++ MLDatasetGenerator/MainPlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+
+namespace MLDatasetGenerator {
+	[BepInPlugin(GUID, Name, Version)]
+	[BepInDependency("cyantist.inscryption.api")]
+	[BepInDependency("io.github.TeamDoodz.TDLib")]
+	[BepInDependency("io.github.TeamDoodz.OmniBackport")]
+	public class MainPlugin : BaseUnityPlugin {
+
+		internal const string GUID = "io.github.TeamDoodz." + Name;
+		internal const string Name = "MLDatasetGenerator";
+		internal const string Version = "1.0.0";
+
+		internal static ManualLogSource logger;
+
+		private void Awake() {
+			logger = Logger;
+			logger.LogMessage($"{Name} v{Version} Loaded!");
+		}
+
+	}
+}
