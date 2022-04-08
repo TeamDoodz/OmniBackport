bac48ae81ef6ad7c3be17e1ab780a5b0320fb397
TeamDoodz <unconfigured@null.spigotmc.org>
2022-04-08 15:21:32 -0500

    looter half-fix

---
+++ OmniBackport/Patchers/LootFix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using DiskCardGame;
+using HarmonyLib;
+
+namespace OmniBackport.Patchers {
+	[HarmonyPatch(typeof(Loot))]
+	[HarmonyPatch("OnDealDamageDirectly")]
+	static class LootFix {
+		static IEnumerator Postfix(IEnumerator sequence, Loot __instance, int amount) {
+			//yield return __instance.PreSuccessfulTriggerSequence();
+			ViewManager.Instance.SwitchToView(View.Hand, false, true);
+			MainPlugin.logger.LogInfo($"Drawing {amount} cards");
+			int num;
+			for(int i = 0; i < amount; i = num + 1) {
+				if(CardDrawPiles3D.Instance.Pile.NumCards > 0) {
+					MainPlugin.logger.LogInfo($"Drawing from main deck");
+					CardDrawPiles3D.Instance.Pile.Draw();
+					yield return CardDrawPiles3D.Instance.DrawCardFromDeck(null, null);
+				} else if (CardDrawPiles3D.Instance.SidePile.NumCards > 0) {
+					MainPlugin.logger.LogInfo($"Drawing from side deck");
+					yield return CardDrawPiles3D.Instance.DrawFromSidePile();
+					CardDrawPiles3D.Instance.SidePile.Draw();
+				} else {
+					MainPlugin.logger.LogInfo($"Not drawing");
+					break;
+				}
+				num = i;
+			}
+			ViewManager.Instance.Controller.LockState = ViewLockState.Unlocked;
+			yield return __instance.LearnAbility(0f);
+			yield break;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -41,6 +41,7 @@ Thanks to Aaron from the Inscryption Modding Discord for helping with some Harmo
 
 ## 0.2.2
 - Fixed an error that would occur if a gemified card tried to draw from the side deck when the side deck was empty.
+- Sort-of fixed the Looter sigil; it animates weirdly now but it will actually update the card piles. It will also now draw from the side deck if the main deck is empty.
 
 ## 0.2.1
 - Dialogue for the Overclock and Gemify nodes will no longer repeat after entering the node more than once.
